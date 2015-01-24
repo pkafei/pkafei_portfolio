0f3f2a1d7277049e60e01b80381d03fbbeb947c2
Portia Burton <plburton@gmail.com>
2015-01-24 15:51:42 -0800

    Added talks.html page

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/open_source', function(req, res) {
 	res.render('os.html');
 });
 
+app.get('/talks', function(req, res) {
+	res.render('talks.html');
+});
+
 app.listen(app.get('port'), function() {
 console.log("Node app is running at localhost:" + app.get('port'));
 });
