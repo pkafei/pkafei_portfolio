b81369e3ebcae5a238985bf454ab2267af80ad55
Portia Burton <plburton@gmail.com>
2015-04-16 19:43:37 -0700

    Added uglify in Grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,12 +23,20 @@ grunt.initConfig({
   jshint: {
     all: ['Gruntfile.js', 'views/js/*.js']
   },
+  uglify: {
+    my_target: {
+      files: {
+        'dest/output.min.js': ['src/input1.js', 'src/input2.js']
+      }
+    }
+  }
 });
 
 //Grunt 
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.registerTask('default', ['concat', 'watch']);
+grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-jshint');
 };
 
